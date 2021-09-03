d1fd442074cbf8f83437b85e710d399c36f3e155
Wideparo <wideparo@mail.ru>
2021-09-03 15:38:07 +0300

    Final

--- main.py
+++ main.py
@@ -18,9 +18,12 @@ async def get_all_books():
     return results
 
 
-@app.post('/books')
-async def create_a_book():
-    pass
+@app.post('/books', response_model=Book, status_code=status.HTTP_201_CREATED)
+async def create_a_book(book: Book):
+    new_book = Book(title=book.title, description=book.description)
+    session.add(new_book)
+    session.commit()
+    return new_book
 
 
 @app.get('/book/{book_id}', response_model=Book)
@@ -32,11 +35,22 @@ async def get_all_books(book_id: int):
     return result
 
 
-@app.put('/book/{book_id}')
-async def get_all_books(book_id: int):
-    pass
+@app.put('/book/{book_id}', response_model=Book)
+async def get_all_books(book_id: int, book: Book):
+    statement = select(Book).where(Book.id == book_id)
+    result = session.exec(statement).first()
+    result.title = book.title
+    result.description = book.description
+    session.commit()
+    return result
 
 
-@app.delete('/book/{book_id}')
+@app.delete('/book/{book_id}', status_code=status.HTTP_204_NO_CONTENT)
 async def delete_a_book(book_id: int):
-    pass
+    statement = select(Book).where(Book.id == book_id)
+
+    result = session.exec(statement).one_or_none()
+    if result == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Resource Not Found")
+    session.delete(result)
+    return result
